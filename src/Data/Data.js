17e6a4b2124df0a0015cc1137a8753f9bb1b25f9
mint <danielavargashidalgo@gmail.com>
2022-04-07 00:27:10 -0500

    map of data err in img

---
+++ src/Data/Data.js
@@ -0,0 +1,36 @@
+import image1 from "../img/react-ecomerce.png"
+const ProjectsData =[
+   {
+      id: 1,
+      title: 'Ecomerce',
+      image:{image1},
+       git:'https://github.com/LDvargasHidalgo/LaMamona', 
+      deploy:"https://la-mamona-e.vercel.app/gallery/pescado",
+      language:"react"
+   },
+   {
+      id: 2,
+      title: 'Agenda',
+      image:'../img/php-agenda.png',
+      git:'https://github.com/LDvargasHidalgo/crud-mvc-php',
+      deploy:"http://crud-php-citas.epizy.com/crud-citas-poo-mvc/index.php",
+      language:"php"
+   },
+   {
+      id: 3,
+      title: 'TodoList',
+      image:'../img/todo-mern.png',
+      git:'https://github.com/LDvargasHidalgo/TodoListMERN',
+      deploy:"https://todolist-mern2022.herokuapp.com/",
+      language:"mern"
+   },
+   {
+      id: 4,
+      title: 'Escuela de música',
+      image:'/toquemEl-Dom',
+      git:'https://github.com/LDvargasHidalgo/ProyectosEducamas/tree/main/JS/proyecto%20de%20musica',    
+      language:"js"
+   },
+];
+
+export default ProjectsData;
